6dc3ca10ee524a3eef38b0776ef29d6ec173ac6b
James Pratt <justafluke.io@gmail.com>
2021-12-05 13:41:20 -0800

    Added tutors to Recently Added Tutors

--- application/node/controller/index.js
+++ application/node/controller/index.js
@@ -14,10 +14,75 @@ const express = require('express');
 const router = express.Router();
 
 const lazyReg = require('../model/lazyRegistration');
+const database = require("../model/mysqlConnection");
 
-router.get('/', lazyReg.removeLazyRegistrationObject, (req, res) => {
+function getCSC(request, response, callback) {
 
-    res.render("landingPage");
+    let query = `SELECT users.user_id,\n` +
+        `       users.first_name,\n` +
+        `       users.last_name,\n` +
+        `       users.major,\n` +
+        `       tutors.tutor_id,\n` +
+        `       tutors.image,\n` +
+        `       tutors.approved,\n` +
+        `       major.major_long_name,\n` +
+        `       major.major_short_name\n` +
+        `FROM tutors\n` +
+        `JOIN users ON users.user_id = tutors.tutor_id\n` +
+        `JOIN major ON users.major = major.major_id\n` +
+        `WHERE major.major_short_name = 'CSC' AND tutors.approved = 1`;
+
+    // Perform the query on the database passing the result to our anonymous callback function.
+    database.query(query, (err, result) => {
+
+        // Append default data to the request before calling callback.
+        request.searchResult = "";
+        request.images = [];
+
+        // If we hit an error with the mysql connection or query we just return the above empty data
+        // since we have no data to display from the database. This should never happen in production.
+        if(err) {
+            console.log(`Encountered an error when performing query: ${query}`);
+        }
+        else {
+
+            // We have received data from the database.
+            // Extract all of the images from the result and convert them from mysql blob to a viewable image.
+            let images = [];
+            for(let i = 0; i < result.length; i++) {
+
+                let image = result[i]['image'];
+                if(image !== null) {
+                    /*
+                    TODO: according to spec this should be a thumbnail. Not sure if
+                     we're supposed to convert here or on upload. Something to ask about?
+                    */
+                    image = Buffer.from(image.toString('base64'));
+                    images.push(image);
+                }
+            }
+
+            // Append the actual data to the request.
+            request.searchResult = result.slice(0, 5); // Only take the first 5 items from the results found.
+            request.images = images.slice(0, 5); // Only take the first 5 items from the results found.
+        }
+
+        callback();
+    });
+}
+
+router.get('/', lazyReg.removeLazyRegistrationObject, getCSC, (req, res) => {
+
+    let searchResult = req.searchResult;
+    if (Array.isArray(searchResult) === false) {
+        searchResult = [];
+    }
+
+    res.render("landingPage", {
+        results: 1,
+        searchResult: searchResult,
+        images: req.images
+    });
 });
 
 
