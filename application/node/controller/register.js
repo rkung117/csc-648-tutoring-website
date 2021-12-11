1e39778166cca627a73724a097eaedd77eb06519
Cameron Robinson <cameron.k.robinson@gmail.com>
2021-12-10 17:19:17 -0800

    Updated register user function to access database.

--- application/node/controller/register.js
+++ application/node/controller/register.js
@@ -8,47 +8,64 @@ const {database, mysql} = require('../model/mysqlConnection');
 
 function registerUser(request, response, callback){
 
-    const user = request.body.name;
     const hashedPassword = loginHashing.hashPasswordForRegistration(request.body.password);
 
-    database.connect(async (err, connection) => {
+    const email = request.body.email;
+    const password_hashed = hashedPassword.hash;
+    const password_salt = hashedPassword.salt;
+    const first_name = request.body.first_name;
+    const last_name = request.body.last_name;
+    const major = request.body.major_dropdown;
 
-        if (err) throw (err);
+    const sqlSearch = "SELECT * FROM users WHERE email = ?";
+    const search_query = mysql.format(sqlSearch,[email]);
+
+    request.registered = false;
+
+    database.query (search_query, async (err, result) => {  
         
-        const sqlSearch = "SELECT * FROM userTable WHERE user = ?";
-        const search_query = mysql.format(sqlSearch,[user]);
-        const sqlInsert = "INSERT INTO userTable VALUES (0,?,?)";
-        const insert_query = mysql.format(sqlInsert,[user, hashedPassword]);
-        // ? will be replaced by values
-        // ?? will be replaced by string 
+        if (err) throw (err)
         
-        await connection.query (search_query, async (err, result) => {  
+        if (result.length != 0) {
+            console.log("------> User already exists");
+
+            // TODO: Set value to say that user email for username already exists?
+            callback()
+        } 
+        else {
+            let majorIdQuery = "SELECT major_id FROM major WHERE major_short_name = ?";
+            majorIdQuery = mysql.format(majorIdQuery,[major]);
             
-            if (err) throw (err)
-            
-            console.log("------> Search Results");
-            console.log(result.length);
-            
-            if (result.length != 0) {
-                connection.release();
-                console.log("------> User already exists");
-                res.sendStatus(409) ;
-            } 
-            else {
-                await connection.query (insert_query, (err, result)=> {   
-                    connection.release(); 
-                    
-                    if (err) throw (err);
-                    console.log ("--------> Created new User");
-                    console.log(result.insertId);
-                    res.sendStatus(201);
-                })
-            }
-        })
+            await database.query (majorIdQuery, async (err, result)=> {  
+                
+                majorId = result[0]['major_id']
+
+                if (result.length > 0) {
+
+                    // TODO: Remove username here after removed from database, this default is being set here until then.
+                    const sqlInsert = "INSERT INTO users (email, username, password_hashed, password_salt, first_name, last_name, major) VALUES (?,?,?,?,?,?,?)";
+                    const insert_query = mysql.format(sqlInsert,[email, "TODO_REMOVE", password_hashed, password_salt, first_name, last_name, majorId]);
+                    await database.query (insert_query, (err, result)=> {   
+                
+                        if (err) throw (err);
+
+                        request.registered = true;
+                        callback()
+                    })
+                }
+                else {
+
+                    // TODO: Set "unknown error" here. This is if the user
+                    // somehow selects an unknown major.
+                    callback()
+                }
+            });
+
+        }
     })
 }
 
-router.get('/', (req, res) => {   
+router.get('/', (req, res) => {
 
     if(req.loginValidated) {
 
@@ -59,7 +76,7 @@ router.get('/', (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+router.post('/', registerUser, (req, res) => {
 
     if(req.registered) {
 
@@ -68,7 +85,7 @@ router.post('/', (req, res) => {
     else {
 
         // TODO: Display error message here.
-    res.render("studentRegister");    
+        res.render("studentRegister");
     }
 });
 
